cc292083ff7b0d3bcb2e853c74e8a14bc06d9c39
lorettalevin <lorettalevin4@gmail.com>
2018-02-22 17:58:39 +0100

    Petition Part 1 and 2 complete

--- db.js
+++ db.js
@@ -4,19 +4,67 @@ var { dbUser, dbPass } = require("./secrets");
 var db = spicedPg(`postgres:${dbUser}:${dbPass}@localhost:5432/signatures`);
 
 function signPetition(first, last, signature) {
-    const q =
-        "INSERT INTO signatures (first, last, signature) VALUES ($1, $2, $3)";
-    const params = [first, last, signature];
-    return db
-        .query(q, params)
-        .then(function(results) {
-            console.log(results.rows); //do a return
-        })
-        .catch(function(err) {
-            console.log(err);
-        });
+    return new Promise(function(resolve, reject) {
+        const q =
+            "INSERT INTO signatures (first, last, signature) VALUES ($1, $2, $3) RETURNING id";
+        const params = [first, last, signature];
+        db
+            .query(q, params)
+            .then(function(results) {
+                resolve(results);
+            })
+            .catch(function(err) {
+                reject(err);
+            });
+    });
 }
 
-db.query;
+function getSigners() {
+    return new Promise(function(resolve, reject) {
+        const q = "SELECT first, last FROM signatures";
+        db
+            .query(q)
+            .then(function(results) {
+                resolve(results.rows);
+            })
+            .catch(function(err) {
+                reject(err);
+            });
+    });
+}
 
-exports.signPetition = signPetition;
+function getSigURL(sigID) {
+    return new Promise(function(resolve, reject) {
+        const q = "SELECT signature FROM signatures WHERE id = $1";
+        const params = [sigID];
+        db
+            .query(q, params)
+            .then(function(results) {
+                resolve(results.rows[0].signature);
+            })
+            .catch(function(err) {
+                reject(err);
+            });
+    });
+}
+
+function getSigCount() {
+    return new Promise(function(resolve, reject) {
+        const q = "SELECT COUNT(*) FROM signatures";
+        db
+            .query(q)
+            .then(function(results) {
+                resolve(results.rows[0].count);
+            })
+            .catch(function(err) {
+                reject(err);
+            });
+    });
+}
+
+module.exports = {
+    getSigURL,
+    signPetition,
+    getSigners,
+    getSigCount
+};
--- index.js
+++ index.js
@@ -3,39 +3,89 @@ const hb = require("express-handlebars");
 const app = express();
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
+const cookieSession = require("cookie-session");
 const db = require("./db");
 const signPetition = db.signPetition;
+const getSigners = db.getSigners;
+const getSigURL = db.getSigURL;
+const getSigCount = db.getSigCount;
+
+const checkForSigID = function(req, res, next) {
+    if (req.session.sigID) {
+        next();
+    } else {
+        res.redirect("/petition");
+    }
+};
 
 app.engine("handlebars", hb());
 app.set("view engine", "handlebars");
 app.use(cookieParser());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(__dirname + "/public"));
+app.use(cookieSession({
+    secret: "a really hard to guess secret",
+    maxAge: 1000 * 60 * 60 * 24 * 14
+}));
 
 app.get("/petition", function(req, res) {
-    res.render("petition", { layout: "main" });
+    if (req.session.sigID) {
+        res.redirect("/signers");
+    } else {
+        res.render("petition", {layout: "main"});
+    }
 });
 
-app.get("/thankyou", function(req, res) {
-    res.render("thankyou", { layout: "main" });
+app.get("/thankyou", checkForSigID, function(req, res) {
+    Promise.all([
+        getSigURL(req.session.sigID),
+        getSigCount()
+    ]).then(function(results) {
+        res.render("thankyou", {
+            layout: "main",
+            signature: results[0],
+            count: results[1]
+        });
+    });
+
+    // getSigURL(req.session.sigID).then(function(signature) {
+    //     getSigCount().then(function(count) {
+    //         res.render("thankyou", {
+    //             layout: "main",
+    //             signature,
+    //             count
+    //         });
+    //     });
+    // });
 });
 
 app.get("/signaturepage", function(req, res) {
-    res.render("signaturepage", { layout: "main" });
+    res.render("signaturepage", {layout: "main"});
+});
+
+app.get("/signers", checkForSigID, function(req, res) {
+    getSigners().then(function(signers) {
+        res.render("signers", {
+            layout: "main",
+            signers
+        });
+    });
 });
 
 app.post("/petition", function(req, res) {
     if (!req.body.first || !req.body.last || !req.body.signature) {
         res.render("petition", {
             layout: "main",
-            error: "You done fucked up."
+            error: "Please fill out ALL the fields."
         });
     } else {
-        signPetition(req.body.first, req.body.last, req.body.signature).then(
-            function() {
-                res.redirect("/thankyou");
-            }
-        );
+        signPetition(req.body.first, req.body.last, req.body.signature).then(function(results) {
+            const sigID = results.rows[0].id;
+            req.session = {
+                sigID
+            };
+            res.redirect("/thankyou");
+        });
     }
 });
 
